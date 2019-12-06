2b37b356f7405a356d20745513cef94ba52d275c
janice-cotcher <janice.cotcher@rbe.sk.ca>
2019-12-06 10:41:11 -0600

    Update card_draw.py
    Export data to a CSV file. Example file provided.

--- card_draw.py
+++ card_draw.py
@@ -1,10 +1,12 @@
 # code adapted from https://medium.com/@anthonytapias/build-a-deck-of-cards-with-oo-python-c41913a744d3
 import random
-import tabulate
+# import tabulate
+import pandas as pd
 # import collections
 
 
 class Card:
+    """Define the suit and value of the card"""
     def __init__(self, suit, val):
         self.suit = suit
         self.value = val
@@ -14,11 +16,13 @@ class Card:
 
 
 class Deck:
+    """Define the deck of 52 cards"""
     def __init__(self):
         self.cards = []
         self.build()
 
     def build(self):
+        """Build the deck of cards"""
         for s in ["♠", "♥", "♣", "♦"]:
             for v in range(2, 11):
                 self.cards.append(Card(s, v))
@@ -26,15 +30,18 @@ class Deck:
                 self.cards.append(Card(s, f))
 
     def shuffle(self):
+        """Shuffle the deck of cards"""
         for i in range(len(self.cards) - 1, 0, -1):
             r = random.randint(0, i)
             self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
 
     def drawCard(self):
+        """Draw a single card"""
         return self.cards.pop()
 
 
 class Trials:
+    """Chose the number of trials"""
     def __init__(self):
         self.hand = []
         self.n = 0
@@ -43,6 +50,7 @@ class Trials:
         return self.hand
 
     def draw(self, deck, n_max):
+        """Draw a card and then replace and shuffle""""
         while self.n < self.n_max:
             deck = Deck()
             deck.shuffle()
@@ -52,6 +60,9 @@ class Trials:
         return self
 
     def count(self, n_max):
+        """Count the number of cards of each type and
+        output the results to a csv file
+        """
         deck = Deck()
         self.draw(deck, n_max)
         total = []
@@ -60,11 +71,16 @@ class Trials:
             count = self.hand.count(card)
             percent = count * 100/self.n_max
             total.append([card, count, percent])
-        print(tabulate.tabulate(total,
-                                headers=["Card", " Count", "% Frequency"],
-                                tablefmt="fancy_grid"))
+        # print(tabulate.tabulate(total,
+        #                         headers=["Card", " Count", "% Frequency"],
+        #                         tablefmt="fancy_grid"))
+        df = pd.DataFrame([[i[0], i[1], i[2]] for i in total],
+                  columns=['Cards', 'Count', '% Frequency'])
+
+        df.to_csv('file.csv', index=False)
 
     def count_input(self):
+        """Ask the user how many trials they want to run"""
         self.n_max = int(input("How many cards would you like to draw? "))
         while True:
             try:
